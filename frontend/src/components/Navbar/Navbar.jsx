5a2d893bbe0b9ca08ac85c1a2ac7365695dfc4c1
aryanaryan <103895665+aryankumar-dev@users.noreply.github.com>
2025-06-16 19:59:04 +0530

    Initial commit with Navbar component

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-light bg-light justify-content-between px-5">
+            <a className="navbar-brand" href="#">HireMe</a>
+
+            <div className="navbar-brand">
+                <a className="navbar-brand" href="#">Saved Jobs</a>
+                <a className="navbar-brand" href="#">Logo</a>
+                <a className="navbar-brand" href="#">Logo</a>
+            </div>
+
+
+
+        </nav>
+    );
+}
+
+export default Navbar;
