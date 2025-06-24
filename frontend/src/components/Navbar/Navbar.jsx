a973e5c9ae6a6ef8b643f5e446f039dc5a319118
aryanaryan <103895665+aryankumar-dev@users.noreply.github.com>
2025-06-24 14:42:18 +0530

    Testing Versal Deployment

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navbar.css'
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-light bg-light justify-content-between px-5">
+        <nav className="navbar navbar-light bg-light justify-content-between px-5 ">
             <a className="navbar-brand" href="#">HireMe</a>
 
             <div className="navbar-brand">
